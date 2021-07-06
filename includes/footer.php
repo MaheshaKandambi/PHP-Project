f8d0fadd52e0a5bed3cf8c61c5d03abf9c2cc860
MaheshaKandambi <maheshakandambi97@gmail.com>
2021-07-07 00:39:16 +0530

    add footer

---
+++ includes/footer.php
@@ -0,0 +1,119 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Pages</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>User Section</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Top Products Categories</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="#">Chocolate</a></li>
+                    <li><a href="#">Chocolate Cake</a></li>
+                    <li><a href="#">Chocolate Ice Creams</a></li>
+                    <li><a href="#">Chocolate Chip Cookies</a></li>
+                    <li><a href="#">Chocolate Pudding</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Find Us</h4>
+                
+                <p><!-- p Start -->
+                    
+                    <strong>Chocolate Shop.lk,</strong>
+                    <br/>Main Street,
+                    <br/>Samagipura, 
+                    <br/>Colombo-7.
+                    <br/><strong>Tel:</strong> 012-3456789/023-4567890
+                    <br/><strong>email:</strong> chocolateshoplk@gmail.com
+ 
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">Check Our Contact Page</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4>Get The News</h4>
+                
+                <p class="text-muted">
+                    Dont miss our latest update products.
+                </p>
+                
+                <form action="" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        
+                        <input type="text" class="form-control" name="email">
+                        
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2021 Chocolate-shop.lk Store All Rights Reserve</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-right"><a href="#">Privacy Policy | Terms of Use </a></p>
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
