da73bd85c7127cd4b2af0883677de3bccf35bc7e
Mahesha Kandambi <58287739+MaheshaKandambi@users.noreply.github.com>
2021-07-08 14:48:33 +0530

    Delete admin_area directory

--- admin_area/admin-dashboard/login-check.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    if(!isset($_SESSION['user'])){ 
-        
-        $_SESSION['no-login-message'] = "<div class='error-text'>Please login to access <br> Admin Panel.</div>";
-        
-        header('location:'.SITEURL.'admin_area/admin-dashboard/login.php');
-    }
-?>
